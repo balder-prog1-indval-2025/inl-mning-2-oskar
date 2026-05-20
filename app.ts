66cd6841bcf973777c9b45ecf3c62335120790a7
oskar <oli0311@studetn.skelleftea.se>
2026-05-20 09:47:52 +0200

    lol

--- app.ts
+++ app.ts
@@ -1,240 +1,107 @@
-let char_x = 100
-let randombred = random(100,W-100)
-let uppner = 0
-let tid=0
-let tid2=0
-let x3 = 0
-let x4 = 0
-let char_y = 400
-let movement_x = 0
-let movement_y = 0
-let jump_time = 0
-let jumping = false
-let hitbox = new Hitbox(char_x, char_y, 100, 100)
-let ippey=1
+import { updateCharacter } from "./app2"
+import { jump } from "./app2"
+import { walk } from "./app2"
+import { updatePosition } from "./app2"
+import { lukazattack } from "./app2"
+import { marrekuben } from "./app2"
+import { reset } from "./app2"
+import { ippeboss1 } from "./app2"
+import { ippeboss } from "./app2"
+import { coins1 } from "./app2"
+import { coins2 } from "./app2"
+import { coins3 } from "./app2"
+import { death1 } from "./app2"
+import { death2 } from "./app2"
+import { win } from "./app2"
+import {  hitbox } from "./app2"
+import {  lukazhb} from "./app2"
+import {  randombred } from "./app2"
+import {  uppner } from "./app2"
+import {  x4} from "./app2"
+import {  char_y } from "./app2"
+import {  char_x } from "./app2"
+import {  updateMovement } from "./app2"
 
-let marremonster = await fetchImage ("media.jpg")
-let ippebosse = await fetchImage ("IPPEBOSS.jpg")
-let ippe2 = await fetchImage ("ippe2.png")
+
+let deathtimer = 0
 let lukazmonster = await fetchImage ("lukaz.jpg")
-let coin = await fetchImage ("8cd3c27a96c00fe6a769ca850758a959.jpg")
 let coinvalue = 0
-function updateCharacter(x: number, y: number, hitbox: Hitbox) {
-    rectangle(x, y, 100, 100)
-    hitbox.x = x
-    hitbox.y = y
-   
-}
- 
-function jump() {
-    if (jumping && char_y <= H/2+H/16) {
-        jump_time += deltaTime
-        return -10 + 10 * jump_time / 700
-    } else if (char_y > H/2+H/16) {
-        jumping = false
-        char_y = H/2+H/16
-        jump_time = 0
-        return 0
-    }
-    return 0
-}
- 
-function walk() {
-    if (keyboard.a && !jumping) {
-        return -5
-    } else if (keyboard.d && !jumping) {
-        return 5
-    } else if (jumping) {
-        return movement_x
-    } else if (movement_x < -0.1) {
-        return movement_x + 0.2
-    } else if (movement_x > 0.1) {
-        return movement_x - 0.2
-    } else {
-        return 0
-    }
-}
- 
-function updatePosition() {
-    if(char_x+movement_x>-50 && char_x+movement_x<W-50){
-    char_x += movement_x
-    }
-    char_y += movement_y
-}
 
-let framme = false
-function lukazattack(){
-    if(uppner<201 && framme == false ){
-        uppner+=1
-        tid2=0
-        }
-    if (uppner > 195 ){
-        rectangle(W-randombred+29, uppner-77,5,H ,"red")
-        rectangle(W-randombred+62, uppner-80,5,H ,"red")
-        tid += 1
-        tid2 = 0
-        if(hitbox.intersects(lukazlazer)){
-            död = true
-        }
-    }
-    if(tid >= 100) {
-        framme = true
-    }  
-    if (uppner>0 && framme ){
-        uppner-=1
-        tid = 0
-        }
-    if(uppner<1){
-        tid2+=1
-     }
-     if(tid2 >= 100) {
-        framme = false
-        randombred = random(100,W-100)
-        lukazhb.x =  W-randombred 
-        lukazlazer.x =  W-randombred + 28 
-    } 
-}
-let vänster = true
-async function marrekuben(speed: number,x2: number, ){
-    /*marremonster = await fetchImage ("marreflip.png")
-    let sprite = new Sprite(marremonster, 1, 1)
-    sprite.height = 10 
-    sprite.width = 10 
-    sprite.x = 10
-    sprite.y = 10
-    sprite.draw()*/
-    
-    let hitboxkub = new Hitbox(W-x4,H/2+H/17,100,100)
-    ctx.drawImage(marremonster, W-x3,H/2+H/17,100,100)
-    
-  
-    if(x4<W+100 && vänster){
-    x4+=speed
-    x3+=speed
-    x2+=speed
-   }
-  if(x4>W+99){
-    vänster = false
-    marremonster = await fetchImage ("marreflip.png")
-
-  }
-  
-  if (hitbox.intersects(hitboxkub)) {
-    död = true
-}
-  
-  
-   if(x4>-1 && vänster==false){
-    x4-=speed
-    x3-=speed
-    x2-=speed
-  }
-  if(x4<-1){
-    vänster = true
-    marremonster = await fetchImage ("Media.jpg")
-
-  }
-
-  console.log()
- return(x3)
- return(x4)
- hitboxkub.x = x4
-}
-
-function reset(){
-     char_x = 100
-     randombred = random(100,W-100)
-     uppner = 0
-     tid=0
-     tid2=0
-     x3 = 0
-     x4 = 0
-     char_y = 400
-     movement_x = 0
-     movement_y = 0
-     jump_time = 0
-     jumping = false
-
-
-}
-function ippeboss(){
-    
-    ctx.drawImage(ippe2, W-W/2-12.5,ippey,25,25)
-    ctx.drawImage(ippebosse, W/6,1,W-W/3,150)
-    let hitboxippe = new Hitbox(W/6,1,W-W/3,150)
-    if (hitbox.intersects(hitboxippe)) {
-        död = true
-    }
-    
-    if (ippey==H){
-        ippey = 1
-    }
-    if (ippey<H){
-        ippey+=1
-    }
-
-}
-    
-
-function coins1(x: number, y: number,){
-    ctx.drawImage(coin, x, y,50,60)
-    let coinhb = new Hitbox(x,y,50,60)
-    if (hitbox.intersects(coinhb) ){
-    return(coinvalue=1)
-    }
-}
-function coins2(x: number, y: number,){
-    ctx.drawImage(coin, x, y,50,60)
-    let coinhb = new Hitbox(x,y,50,60)
-    if (hitbox.intersects(coinhb) ){
-    return(coinvalue=2)
-    }
-}
-function coins3(x: number, y: number,){
-    ctx.drawImage(coin, x, y,50,60)
-    let coinhb = new Hitbox(x,y,50,60)
-    if (hitbox.intersects(coinhb) ){
-    return(coinvalue=3)
-    }
-    
-}
-
-let lukazlazer = new Hitbox(W-randombred+28, 125,37,H)
-let lukazhb = new Hitbox(W-randombred,H-H-200+uppner,100,200)
-
-function death(): void{
-    rectangle(0,0,10000,10000)
-    text('WASTED', W - W / 2 - 475, 400, 300, 'red')
-}
-let död = false
-let bana1 = false
+let död1 = false
+let död2 = false
+let bana1 = true
 let bana2 = false
 let bana3 = false
-let ippebossbana = true
+let ippebossbana1 = false
+let ippebossbana2 = false
+let winner =false
 update = () => {
-    if (död){
-        death()
-     
+    if (död1){
+        death1(bana1,bana2,bana3,ippebossbana1,ippebossbana2,död2)
+        
     }else{
+        clear()
         if(bana1){
-
-            clear()
+            deathtimer = 0
+            
             
             ctx.drawImage(lukazmonster, W-randombred,H-H-200+uppner,100,200)
             
             
-        movement_x = walk()
-        movement_y = jump()
-        if (keyboard.w) {
-            jumping = true
+            updateMovement()
+            if (hitbox.intersects(lukazhb)) {
+                död1 = true
+            }
+            if( död1 == false){
+                lukazattack(död1)
+                marrekuben(2,W+x4,död1)
+                
+            }
+            
+            updatePosition()
+            updateCharacter(char_x, char_y, hitbox)
+            
+            
+            lukazhb.y = uppner-200
+            if(keyboard.l){
+                
+                
+            }
+            if(coinvalue == 0){
+                coins1(W/4,H/2,coinvalue)
+            }
+            if(coinvalue == 1){
+                coins2(W-W/4,H/5,coinvalue)
+            }
+            if(coinvalue == 2){
+                coins3(W/2,H/3,coinvalue)
+            }
+            if(coinvalue == 3){
+                
+                coinvalue = 4
+                bana2 = true
+                
+         
+         
         }
-        if (hitbox.intersects(lukazhb)) {
-            död = true
+        
+        
+    }
+    if(bana2){
+        
+        bana1=false
+        
+        ctx.drawImage(lukazmonster, W-randombred,H-H-200+uppner,100,200)
+        
+        
+        updateMovement()
+        if(hitbox.intersects(lukazhb)) {
+            död1= true
         }
-        if( död == false){
-        lukazattack()
-        marrekuben(2,W+x4)
-       
+        if( död1 == false){
+            lukazattack(död1)
+            marrekuben(4,W/2,död1)
+            
         }
         
         updatePosition()
@@ -242,178 +109,131 @@ update = () => {
         
         
         lukazhb.y = uppner-200
-        if(keyboard.l){
-         
         
-        }
+        
         if(coinvalue == 0){
-            coins1(W/4,H/2)
+            coins1(W/4,H/2,coinvalue)
         }
         if(coinvalue == 1){
-            coins2(W-W/4,H/5)
+            coins2(W-W/9,H/5,coinvalue)
         }
         if(coinvalue == 2){
-            coins3(W/2,H/3)
+            coins3(W/6,H/4,coinvalue)
         }
         if(coinvalue == 3){
-            
+            bana3 = true
             coinvalue = 4
-         bana2 = true
-        
-         console.log
+        }
+        if(coinvalue == 4){
+            coinvalue = 0
+            
+            
+        }
+    }
+    
+    if(bana3){
            
-        }
-        
-        
-        }
-        if(bana2){
-             clear()
-           bana1=false 
+           bana2=false
            
            ctx.drawImage(lukazmonster, W-randombred,H-H-200+uppner,100,200)
            
            
-       movement_x = walk()
-       movement_y = jump()
-       if (keyboard.w) {
-           jumping = true
-       }
-       if(hitbox.intersects(lukazhb)) {
-           död = true
-       }
-       if( död == false){
-       lukazattack()
-       marrekuben(4,W/2)
-       
-       }
-       
-       updatePosition()
-       updateCharacter(char_x, char_y, hitbox)
-       
-       
-       lukazhb.y = uppner-200
-       
-        
-       if(coinvalue == 0){
-        coins1(W/4,H/2)
-     }
-     if(coinvalue == 1){
-        coins2(W-W/9,H/5)
-     }
-     if(coinvalue == 2){
-        coins3(W/6,H/4)
-     }
-     if(coinvalue == 3){
-        bana3 = true
-        coinvalue = 4
-     }
-       if(coinvalue == 4){
-       coinvalue = 0
-       
-          
-       }
-       }
-       
-       if(bana3){
-           clear()
-           bana2=false 
-           
-           ctx.drawImage(lukazmonster, W-randombred,H-H-200+uppner,100,200)
-           
-           
-       movement_x = walk()
-       movement_y = jump()
-       if (keyboard.w) {
-           jumping = true
-       }
-       if ( hitbox.intersects(lukazhb)) {
-           död = true
-       }
-       if( död == false){
-       lukazattack()
-       
-       marrekuben(10,W/2)
-       
-       }
-       
-       updatePosition()
-       updateCharacter(char_x, char_y, hitbox)
-       
-       
-       lukazhb.y = uppner-200
-       if(keyboard.l){
-        
+           updateMovement()
+            if ( hitbox.intersects(lukazhb)) {
+                död1 = true
+            }
+            if( död1 == false){
+                lukazattack(död1)
+                
+                marrekuben(10,W/2,död1)
+                
+            }
+            
+            updatePosition()
+            updateCharacter(char_x, char_y, hitbox)
+            
+            
+            lukazhb.y = uppner-200
+            if(keyboard.l){
+                
        
        }
        if(coinvalue == 0){
-        coins1(W/4,H/2)
+        coins1(W/4,H/2,coinvalue)
      }
      if(coinvalue == 1){
-        coins2(W-W/9,H/5)
+        coins2(W-W/9,H/5,coinvalue)
      }
      if(coinvalue == 2){
-        coins3(W/6,H/4)
+        coins3(W/6,H/4,coinvalue)
      }
      if(coinvalue == 3){
         reset()
         coinvalue = 4
-     ippebossbana = true
-    
-     console.log
+     ippebossbana1 = true
+   
+     
        
     }
        if(coinvalue == 4){
            
        coinvalue = 0
-
+ 
        }
-
+ 
        }
-
-     if(ippebossbana){
-        clear()
-        bana2=false 
-        movement_x = walk()
-        movement_y = jump()
-        if (keyboard.w) {
-            jumping = true
-        }
+    if (död2){
+        death2(bana1,bana2,bana3,ippebossbana1,ippebossbana2,död2)
+        
+    }else{
+ 
+     if (ippebossbana1){
+        
+        bana3=false
+        updateMovement()
         updatePosition()
         updateCharacter(char_x, char_y, hitbox)
-        if( död == false){
-            ippeboss()
+        if( död1 == false){
+            ippeboss1(ippebossbana2)
             }
+ 
+     }
+
+     
+     
+     
+     
+     if(ippebossbana2){
+         
+         ippebossbana1=false
+         updateMovement()
+            updatePosition()
+            updateCharacter(char_x, char_y, hitbox)
+            if( död1 == false){
+                ippeboss(död2,winner)
+            }
+            
+        }
+        
+        
+     if(winner){
+        ippebossbana2=false
+        updateMovement()
+           updatePosition()
+           updateCharacter(char_x, char_y, hitbox)
+           if( död1 == false){
+               win()
+           }
 
      }
 
 
-
-
+    }
        }
-        
-    
-    
-    
-    
-    
     }
 
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
+ 
+ 
+   
  
 export { }
---
+++ app2.ts
@@ -0,0 +1,473 @@
+
+export let char_x = 100
+var vinstljudet = new Audio('[FNAF] 6 am - QuickSounds.com.mp3');
+var dödsljudet = new Audio('gta-v-wasted-death-sound.mp3');
+export let randombred = random(100,W-100)
+export let uppner = 0
+let tid=0
+let tid2=0
+let x3 = 0
+export let x4 = 0
+export let char_y = 400
+export let movement_x = 0
+export let movement_y = 0
+let jump_time = 0
+export let jumping = false
+export let hitbox = new Hitbox(char_x, char_y, 100, 100)
+let ippebosse = await fetchImage ("IPPEBOSS.jpg")
+let ippe = [
+    {
+        "x": W/2,
+        "y": 1,
+        "v":random(-9,9)/10,
+        "m": random(15,30)/10
+    },
+    {
+        "x": W/2,
+        "y": 1,
+        "v":random(-9,9)/10,
+        "m": random(15,30)/10
+    },
+    {
+        "x": W/2,
+        "y": 1,
+        "v":random(-9,9)/10,
+        "m": random(15,30)/10
+    },
+    {
+        "x": W/2,
+        "y": 1,
+        "v":random(-9,9)/10,
+        "m": random(15,30)/10
+    },
+    {
+        "x": W/2,
+        "y": 1,
+        "v":random(-9,9)/10,
+        "m": random(15,30)/10
+    },
+]
+let ippereset = [
+    {
+        "x": W/2,
+        "y": 1,
+        "v":random(-9,9)/10,
+        "m": random(15,30)/10
+    },
+    {
+        "x": W/2,
+        "y": 1,
+        "v":random(-9,9)/10,
+        "m": random(15,30)/10
+    },
+    {
+        "x": W/2,
+        "y": 1,
+        "v":random(-9,9)/10,
+        "m": random(15,30)/10
+    },
+    {
+        "x": W/2,
+        "y": 1,
+        "v":random(-9,9)/10,
+        "m": random(15,30)/10
+    },
+    {
+        "x": W/2,
+        "y": 1,
+        "v":random(-9,9)/10,
+        "m": random(15,30)/10
+    },
+]
+let  h=-150
+let woman3 = await fetchImage ("OIP3.jpg")
+let woman4 = await fetchImage ("OIP.jpg")
+let woman5 = await fetchImage ("OIP2.jpg")
+let woman6 = await fetchImage ("OIP4.jpg")
+let woman2 = [
+    {
+        "x": W,
+        "y": H/2-H/17,
+        "h": W-201,
+        "j": W-195,
+        "k": W-203,
+        "ö": W+200
+        
+    },
+    {
+        "x": -50,
+        "y": H/2-H/17,
+        "h": 201,
+        "j": 195,
+        "k": 203,
+        "ö":-200
+    }
+    
+    
+]
+let woman2reset = [
+    {
+        "x": W,
+        "y": H/2-H/17,
+        "h": W-201,
+        "j": W-195,
+        "k": W-203,
+        "ö": W+200
+        
+    },
+    {
+        "x": -50,
+        "y": H/2-H/17,
+        "h": 201,
+        "j": 195,
+        "k": 203,
+        "ö":-200
+    }
+    
+    
+]
+let marremonster = await fetchImage ("media.jpg")
+let marretimerx = 0
+let tomat=await fetchImage ("ippe2.png")
+let coin = await fetchImage ("8cd3c27a96c00fe6a769ca850758a959.jpg")
+var boingljudet = new Audio('klasky-csupo-boing-sound-effect.mp3');
+
+
+
+export function jump() {
+    if (jumping && char_y <= H/2+H/16) {
+        jump_time += deltaTime
+        return -10 + 10 * jump_time / 700
+        
+    } else if (char_y > H/2+H/16) {
+        jumping = false
+        char_y = H/2+H/16
+        jump_time = 0
+        
+        return 0
+    }
+    return 0
+}
+
+export function updateCharacter(x: number, y: number, hitbox: Hitbox) {
+    rectangle(x, y, 100, 100)
+    hitbox.x = x
+    hitbox.y = y
+   
+}
+export function walk() {
+    if (keyboard.a && !jumping) {
+        return -5
+    } else if (keyboard.d && !jumping) {
+        return 5
+    } else if (jumping) {
+        return movement_x
+    } else if (movement_x < -0.1) {
+        return movement_x + 0.2
+    } else if (movement_x > 0.1) {
+        return movement_x - 0.2
+    } else {
+        return 0
+    }
+}
+
+export function updateMovement() {
+    movement_x = walk()
+    movement_y = jump()
+    if (keyboard.w) {
+        jumping = true
+        boingljudet.play();
+    }
+}
+ 
+export function updatePosition() {
+    if(char_x+movement_x>-50 && char_x+movement_x<W-50){
+    char_x += movement_x
+    }
+    char_y += movement_y
+}
+ 
+let framme = false
+export function lukazattack(död1){
+    if(uppner<201 && framme == false ){
+        uppner+=1
+        tid2=0
+        }
+    if (uppner > 195 ){
+        rectangle(W-randombred+29, uppner-77,5,H ,"red")
+        rectangle(W-randombred+62, uppner-80,5,H ,"red")
+        tid += 1
+        tid2 = 0
+        if(hitbox.intersects(lukazlazer)){
+            död1 = true
+        }
+    }
+    if(tid >= 100) {
+        framme = true
+    }  
+    if (uppner>0 && framme ){
+        uppner-=1
+        tid = 0
+        }
+    if(uppner<1){
+        tid2+=1
+     }
+     if(tid2 >= 100) {
+        framme = false
+        randombred = random(100,W-100)
+        lukazhb.x =  W-randombred
+        lukazlazer.x =  W-randombred + 28
+    }
+}
+let vänster = true
+export async function marrekuben(speed: number,x2: number,död1 ){
+ 
+   
+    let hitboxkub = new Hitbox(W-x4,H/2+H/17,100,100)
+    ctx.drawImage(marremonster, W-x3,H/2+H/17,100,100)
+   
+ 
+    if(x4<W+100 && vänster){
+    x4+=speed
+    x3+=speed
+    x2+=speed
+   }
+  if(x4>W+99){
+    vänster = false
+    marremonster = await fetchImage ("marreflip.png")
+ 
+  }
+ 
+  if (hitbox.intersects(hitboxkub)) {
+    död1 = true
+}
+   if(x4>-1 && vänster==false){
+    x4-=speed
+    x3-=speed
+    x2-=speed
+  }
+  if(x4<-1){
+    vänster = true
+    marremonster = await fetchImage ("Media.jpg")
+ 
+  }
+ 
+  
+ return(x3)
+ return(x4)
+ hitboxkub.x = x4
+}
+
+
+let womanv = true
+export function woman(woman,död2){
+   let hitboxwoman = new Hitbox(woman.x,woman.y,75,200)
+   hitboxwoman.x = woman.x
+   hitboxwoman.y = woman.y
+   if (womanv && woman.x > woman.h){
+    ctx.drawImage(woman3, woman.x,woman.y,75,200)
+    woman.x -= 1
+   }
+   if (womanv && woman.x < woman.j){
+    
+    woman.x-=0.1
+    ctx.drawImage(woman4, woman.x,woman.y,70,200)
+
+   }
+   if(woman.x < woman.j ){
+
+    womanv=false
+   }
+   if (womanv ==false){
+    woman.x+=2
+    ctx.drawImage(woman5, woman.x,woman.y,70,200)
+
+   }
+   if(woman.x > woman.ö ){
+
+    womanv=true
+   }
+   if(hitbox.intersects(hitboxwoman)) {
+    död2 = true
+}
+}
+export function woman9(woman,död2){
+    let hitboxwoman = new Hitbox(woman.x,woman.y,75,200)
+    hitboxwoman.x = woman.x
+    hitboxwoman.y = woman.y
+    if (womanv && woman.x < woman.h){
+     ctx.drawImage(woman6, woman.x,woman.y,75,200)
+     woman.x += 1
+    }
+    if (womanv && woman.x > woman.j){
+     
+     woman.x+=0.1
+     ctx.drawImage(woman5, woman.x,woman.y,70,200)
+ 
+    }
+    if(woman.x > woman.j ){
+ 
+     womanv=false
+    }
+    if (womanv ==false){
+     woman.x-=2
+     ctx.drawImage(woman4, woman.x,woman.y,70,200)
+ 
+    }
+    if(woman.x < woman.ö ){
+ 
+     womanv=true
+    }
+    if(hitbox.intersects(hitboxwoman)) {
+     död2 = true
+ }
+ }
+export function reset(){
+     char_x = 100
+     randombred = random(100,W-100)
+     uppner = 0
+     tid=0
+     tid2=0
+     x3 = 0
+     x4 = 0
+     char_y = 400
+     movement_x = 0
+     movement_y = 0
+     jump_time = 0
+     jumping = false
+     lukazhb.x =  W-randombred
+     lukazlazer.x =  W-randombred + 28
+     ippe = ippereset
+     woman2 = woman2reset
+     
+ 
+ 
+}
+ export function ippeboss1(ippebossbana2){
+   
+    ctx.drawImage(ippebosse, W/6,h,W-W/3,150)
+    if(h<2){
+    h+=1
+    
+    }
+    if(h==0){
+    ippebossbana2=true
+    }
+    if(h==1){
+        h=-150
+        }
+ }
+ 
+export function ippeboss(död2,winner){
+   
+   
+    let hitboxippe = new Hitbox(W/6,1,W-W/3,150)
+    if (hitbox.intersects(hitboxippe) ) {
+        död2 = true
+    }
+    
+   
+    if(död2 == false){
+        ippetomat(ippe[0],död2)
+        ippetomat(ippe[1],död2)
+        ippetomat(ippe[2],död2)
+        ippetomat(ippe[3],död2)
+        ippetomat(ippe[4],död2)
+        woman(woman2[0],död2)
+        woman9(woman2[1],död2)
+        marretimerx+=1
+        
+    }
+    if (marretimerx>2000){
+        marrekuben(2,W+x4,död2)
+
+    }
+    if (marretimerx>4500){
+        winner = true
+
+    }
+    ctx.drawImage(ippebosse, W/6,1,W-W/3,150)
+   
+}
+ 
+export function ippetomat(ippe,död2){
+    ctx.drawImage(tomat, ippe.x,ippe.y,25,25)
+if (ippe.y>H){
+        ippe.y = 1
+        ippe.x=W/2
+        ippe.v = random(-0.9,0.9)
+        
+    }
+    if (ippe.y<H){
+        ippe.y+=ippe.m
+       
+        ippe.x+=ippe.v
+    }
+    let hitboxippe2 = new Hitbox(ippe.x,ippe.y,25,25)
+    if(hitbox.intersects(hitboxippe2)) {
+        död2 = true
+    }
+}
+ 
+export function coins1(x: number, y: number,coinvalue){
+    ctx.drawImage(coin, x, y,50,60)
+    let coinhb = new Hitbox(x,y,50,60)
+    if (hitbox.intersects(coinhb) ){
+    return(coinvalue=1)
+    }
+}
+export function coins2(x: number, y: number,coinvalue){
+    ctx.drawImage(coin, x, y,50,60)
+    let coinhb = new Hitbox(x,y,50,60)
+    if (hitbox.intersects(coinhb) ){
+    return(coinvalue=2)
+    }
+}
+export function coins3(x: number, y: number,coinvalue){
+    ctx.drawImage(coin, x, y,50,60)
+    let coinhb = new Hitbox(x,y,50,60)
+    if (hitbox.intersects(coinhb) ){
+    return(coinvalue=3)
+    }
+   
+}
+ 
+let lukazlazer = new Hitbox(W-randombred+28, 125,37,H)
+export let lukazhb = new Hitbox(W-randombred,H-H-200+uppner,100,200)
+ 
+export function death1(bana1,bana2,bana3,ippebossbana1,ippebossbana2,död1): void{
+    rectangle(0,0,10000,10000)
+    text('WASTED', W - W / 2 - 475, 400, 300, 'red')
+    text('Press Enter to restart', W  - 375, H-30, 25, 'red')
+    dödsljudet.play();
+    if (keyboard.enter){
+
+        död1 = false
+        bana1 = true
+        bana2 = false
+        bana3 = false
+        reset()
+    }
+    
+}
+export function death2(bana1,bana2,bana3,ippebossbana1,ippebossbana2,död2): void{
+    rectangle(0,0,10000,10000)
+    text('WASTED', W - W / 2 - 475, 400, 300, 'red')
+    text('Press Enter to restart', W  - 375, H-30, 25, 'red')
+    dödsljudet.play();
+    if (keyboard.enter){
+        bana1 = false
+        bana2 = false
+        bana3 = false
+        ippebossbana1 = true
+        ippebossbana2 = false
+        reset()
+        död2 = false
+    }
+    
+}
+export function win(){
+    vinstljudet.play();
+    text('victory', W - W / 2 - 550, 400, 300, 'yellow')
+    
+}
